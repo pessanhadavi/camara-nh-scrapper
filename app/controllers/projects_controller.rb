4ff92158a6375f4cc255b0802e93671f9f2cdca2
David Pessanha <davidppessanha@gmail.com>
2021-08-13 11:11:18 -0300

    Created projects#show and projects#set_project methods

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,16 @@
 class ProjectsController < ApplicationController
+  before_action :set_project, only: [:show]
+
   def index
     @projects = Project.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_project
+    @project = Project.find(params[:id])
   end
 end
